dcbcbe4eedac9bfc4c9829b4dfc3b4370603f82e
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-09-27 18:47:23 -0400

    start nmp.js

---
+++ lecture/npm.js
@@ -0,0 +1,30 @@
+// 1 -- Starting the Initialization
+
+// npm init
+
+// This will start the setup process and prompt you with several questions. You can either accept the default values by pressing Enter or provide your custom inputs.
+
+// 2 --  Package Name:
+
+// Copy
+// package name: (my-project)
+// The default name will be the directory's name where the project is initialized, but you can change it to something more specific. For example, if your project is an API service, you might want to name it:
+
+// Copy
+// package name: api-service
+
+// Let's say you want to set up a project for a user authentication API. Here’s how you could customize the prompts:
+
+// npm init
+
+// bashCopy codepackage name: auth-api
+// version: 1.0.0
+// description: A Node.js API for user authentication
+// entry point: app.js
+// test command: jest
+// git repository: https://github.com/yourusername/auth-api.git
+// keywords: authentication, API, Node.js
+// author: Ali
+// license: MIT
+
+// Running npm init -y skips this step-by-step process and uses default values, but customizing the process gives you more control over how your project is set up.
